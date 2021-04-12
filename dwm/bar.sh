a2da44d4adf8b5907d51af6ae0d6c89039746ee0
Simone Pozzebon <77239939+simopozze@users.noreply.github.com>
2021-04-12 20:33:08 +0200

    Add files via upload

--- dwm/bar.sh
+++ dwm/bar.sh
@@ -1,4 +1,4 @@
 while true; do
-	xsetroot -name " [$(date +"%k:%M:%S")] [$(date +"%d/%m/%y")] [storage: $(df /dev/sda4 | grep / | awk '{print $5}')]"    
+	xsetroot -name "$(date +"%k:%M:%S")   $(date +"%d/%m/%y")   $(df /dev/sda4 | grep / | awk '{print $5}')   $(amixer get Master | awk -F'[][]' 'END{ print $4":"$2 }') "    
 	sleep 1s
 done &
