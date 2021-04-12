9e14cdb18fa20a73386e999eec99f54f100a5c6a
Simone Pozzebon <77239939+simopozze@users.noreply.github.com>
2021-04-12 20:34:38 +0200

    Delete bar.sh

--- dwm/bar.sh
+++
@@ -1,4 +0,0 @@
-while true; do
-	xsetroot -name "$(date +"%k:%M:%S")   $(date +"%d/%m/%y")   $(df /dev/sda4 | grep / | awk '{print $5}')   $(amixer get Master | awk -F'[][]' 'END{ print $4":"$2 }') "    
-	sleep 1s
-done &
